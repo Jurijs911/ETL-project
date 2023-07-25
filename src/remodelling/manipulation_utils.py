05bf7af1524d3c859e4b42481f99a525fcae18d1
CParso <cameronparsonage09@gmail.com>
2023-07-25 16:24:34 +0100

    create file structure for remodelling tasks

---
+++ src/remodelling/manipulation_utils.py
@@ -0,0 +1,11 @@
+"""
+Receives data from csv_reader and manipulates it to match the final database schema
+
+format_fact_sales_order
+format_dim_staff
+format_dim_location
+format_dim_design
+format_dim_date
+format_dim_currency
+format_dim_counterparty
+"""
