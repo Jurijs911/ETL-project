2619198934a68d2dfae0c8ada13ee0c59fb3afb8
Matthew Heath <mj.heath@outlook.com>
2023-07-26 11:26:38 +0100

    updates terraform file to add first timestamp file

--- terraform/s3_creation.tf
+++ terraform/s3_creation.tf
@@ -8,6 +8,6 @@ resource "aws_s3_bucket" "data_bucket" {
 
 resource "aws_s3_object" "created_at_date_address" {
   bucket = aws_s3_bucket.ingestion_bucket.bucket
-  key    = "created_at.txt"
+  key    = "address/created_at.txt"
   source = "data/created_at.txt"
 }
