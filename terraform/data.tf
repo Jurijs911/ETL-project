9f8b0bd9ae17725f1b2baf82fa176348526808e8
CParso <cameronparsonage09@gmail.com>
2023-07-27 14:02:54 +0100

    refactor and add template code for other lambdas

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,20 +4,21 @@ data "aws_region" "current" {}
 
 # data "archive_file" "ingestion_lambda_code" {
 #   type        = "zip"
-#   source_file = "${path.module}/../src/file_reader/reader.py" # UPDATE PATH
-#   output_path = "${path.module}/../function.zip"              # UPDATE PATH
+#   output_path = "${path.module}/../ingestion.zip"
+#   source_dir  = "${path.module}/../src/ingestion"
+#   depends_on  = [null_resource.install_dependencies]
 # }
 
 data "archive_file" "remodelling_lambda_code" {
   type        = "zip"
-  output_path = "${path.module}/../remodelling.zip" # UPDATE PATH
+  output_path = "${path.module}/../remodelling.zip"
   source_dir  = "${path.module}/../src/remodelling"
   depends_on  = [null_resource.install_dependencies]
-  excludes = ["${path.module}/../src/remodelling/package"]
 }
 
 # data "archive_file" "loading_lambda_code" {
 #   type        = "zip"
-#   source_file = "${path.module}/../src/file_reader/reader.py" # UPDATE PATH
-#   output_path = "${path.module}/../function.zip"              # UPDATE PATH
+#   output_path = "${path.module}/../loading.zip"
+#   source_dir  = "${path.module}/../src/loading"
+#   depends_on  = [null_resource.install_dependencies]
 # }
