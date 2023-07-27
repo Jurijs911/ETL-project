d87aa6db1eb278c4efe930062cce12ecb108505a
Jurijs911 <jurijs.kosticins@gmail.com>
2023-07-27 14:11:37 +0100

    updated find_most_recent_time

--- src/ingestion_lambda/utils/find_most_recent_time.py
+++ src/ingestion_lambda/utils/find_most_recent_time.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def find_most_recent_time(data):
     """
     arguments:
@@ -11,17 +12,16 @@ def find_most_recent_time(data):
 
         most_recent_date: a datetime of the most recent date
     """
-    
+
     input_rows = data
     most_recent_date = datetime.datetime(1000, 1, 1, 1, 00, 0, 0)
 
     for row in input_rows:
         for key in row:
-            if isinstance(key, datetime.date):
-                if key > most_recent_date:
-                    most_recent_date = key
+            if isinstance(row[key], datetime.date):
+                if row[key] > most_recent_date:
+                    most_recent_date = row[key]
 
     formatted_return_date = datetime.datetime.strftime(
         most_recent_date, '%Y-%m-%d %H:%M:%S.%f')
-
     return formatted_return_date
