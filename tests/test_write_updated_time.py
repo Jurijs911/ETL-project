d0128ed751834e5ad2ef61a7716965d555b15e66
CParso <cameronparsonage09@gmail.com>
2023-07-27 15:51:39 +0100

    fix import statement in write_updated_time test

--- tests/test_write_updated_time.py
+++ tests/test_write_updated_time.py
@@ -1,18 +1,16 @@
-from src.ingestion_lambda.utils.write_updated_time \
-    import write_updated_time
+from src.ingestion_lambda.write_updated_time import write_updated_time
 from moto import mock_s3
 import boto3
 
 
 @mock_s3
 def test_write_updated_time_returns_status_code_when_successfully_uploading():
-
     s3_client = boto3.client("s3")
     s3_client.create_bucket(
-        Bucket='kp-northcoder-ingestion-bucket',
-        CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'},
+        Bucket="kp-northcoder-ingestion-bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
 
     write_response = write_updated_time("test_timestamp", "test_table")
 
-    assert write_response['ResponseMetadata']['HTTPStatusCode'] == 200
+    assert write_response["ResponseMetadata"]["HTTPStatusCode"] == 200
