6182ef73e076fdfed2b0d1f8e536c96894f27195
Matthew Heath <mj.heath@outlook.com>
2023-07-27 11:39:48 +0100

    refactors to pass flake test

--- tests/test_get_currency_add.py
+++ tests/test_get_currency_add.py
@@ -1,29 +1,25 @@
-from pg8000 import VARCHAR
-from requests import patch
 from src.ingestion_lambda.utils.get_currency_add import get_currency_add
-from unittest.mock import patch, Mock
-from unittest import mock
-import pg8000.native
+from unittest.mock import patch
 import datetime
-import pytest
-from decimal import Decimal
+
+ingestion_utils_path = 'src.ingestion_lambda.utils.'
+currency_get_last_time_path = 'get_currency_add.get_last_time'
+get_last_time_patch_path = ingestion_utils_path + \
+    currency_get_last_time_path
 
 
 def test_get_currency_add_returns_list():
-    with patch('src.ingestion_lambda.utils.get_currency_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
             '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
-        # test that we are returning a list,
         result = get_currency_add()
-        # print(result)
         assert isinstance(result, list)
 
 
 def test_gget_currency_add_returns_correct_keys():
-    with patch('src.ingestion_lambda.utils.get_currency_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
             '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
-        # test that we have the correct keys
         expected_keys = {
             "currency_id", "currency_code", "created_at", "last_updated"}
         result = get_currency_add()
@@ -31,10 +27,9 @@ def test_gget_currency_add_returns_correct_keys():
 
 
 def test_get_currency_add_has_correct_value_types():
-    with patch('src.ingestion_lambda.utils.get_currency_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
             '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
-    # test that each key has the correct type value
         result = get_currency_add()
         for item in result:
             assert isinstance(item['currency_id'], int)
@@ -43,10 +38,8 @@ def test_get_currency_add_has_correct_value_types():
             assert isinstance(item['last_updated'], datetime.date)
 
 
-
 def test_get_currency_add_calls_get_last_time():
-    # test that the SQL query calls get_last_time()
-    with patch('src.ingestion_lambda.utils.get_currency_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
             '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
         get_currency_add()
