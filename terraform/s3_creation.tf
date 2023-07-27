b7ff6272b6c472ec0fe35ac24b01539df58c0df6
CParso <cameronparsonage09@gmail.com>
2023-07-27 12:32:52 +0100

    update s3_creation.tf to include a notification to trigger remodelling lambda when ingestion bucket is updated

--- terraform/s3_creation.tf
+++ terraform/s3_creation.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "ingestion_bucket" {
-    bucket = "kp-northcoder-ingestion-bucket"
+  bucket = "kp-northcoders-ingestion-bucket"
 }
 
-resource "aws_s3_bucket" "data_bucket" {
-    bucket = "kp-northcoders-data-bucket"
+resource "aws_s3_bucket" "processed_bucket" {
+  bucket = "kp-northcoders-processed-bucket"
 }
 
 resource "aws_s3_object" "created_at_date_address" {
@@ -58,4 +58,15 @@ resource "aws_s3_object" "created_at_date_transaction" {
   bucket = aws_s3_bucket.ingestion_bucket.bucket
   key    = "transaction/created_at.txt"
   source = "data/created_at.txt"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.data_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.ingestion_bucket.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
 }
