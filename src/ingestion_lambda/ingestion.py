e5c47c95477cff9fa9415e8a4acd83baeaa0436f
CParso <cameronparsonage09@gmail.com>
2023-07-27 15:40:07 +0100

    update ingestion.py to group util calls under a handler function

--- src/ingestion_lambda/ingestion.py
+++ src/ingestion_lambda/ingestion.py
@@ -12,71 +12,76 @@ from src.ingestion_lambda.get_sales_order_add import get_sales_order_add
 from src.ingestion_lambda.get_staff_add import get_staff_add
 
 
-address_data = get_address_add()
+def lambda_handler(event, context):
+    address_data = get_address_add()
 
-if len(address_data) > 0:
-    updated_timestamp = find_most_recent_time(address_data)
-    upload_csv(address_data, "address", "kp-northcoder-ingestion-bucket")
-    write_updated_time(updated_timestamp, "address")
+    if len(address_data) > 0:
+        updated_timestamp = find_most_recent_time(address_data)
+        upload_csv(address_data, "address", "kp-northcoder-ingestion-bucket")
+        write_updated_time(updated_timestamp, "address")
 
-counterparty_data = get_counterparty_add()
+    counterparty_data = get_counterparty_add()
 
-if len(counterparty_data) > 0:
-    updated_timestamp = find_most_recent_time(counterparty_data)
-    upload_csv(
-        counterparty_data, "counterparty", "kp-northcoder-ingestion-bucket"
-    )
-    write_updated_time(updated_timestamp, "counterparty")
+    if len(counterparty_data) > 0:
+        updated_timestamp = find_most_recent_time(counterparty_data)
+        upload_csv(
+            counterparty_data, "counterparty", "kp-northcoder-ingestion-bucket"
+        )
+        write_updated_time(updated_timestamp, "counterparty")
 
-currency_data = get_currency_add()
+    currency_data = get_currency_add()
 
-if len(currency_data) > 0:
-    updated_timestamp = find_most_recent_time(currency_data)
-    upload_csv(currency_data, "currency", "kp-northcoder-ingestion-bucket")
-    write_updated_time(updated_timestamp, "currency")
+    if len(currency_data) > 0:
+        updated_timestamp = find_most_recent_time(currency_data)
+        upload_csv(currency_data, "currency", "kp-northcoder-ingestion-bucket")
+        write_updated_time(updated_timestamp, "currency")
 
-department_data = get_department_add()
+    department_data = get_department_add()
 
-if len(department_data) > 0:
-    updated_timestamp = find_most_recent_time(department_data)
-    upload_csv(department_data, "department", "kp-northcoder-ingestion-bucket")
-    write_updated_time(updated_timestamp, "department")
+    if len(department_data) > 0:
+        updated_timestamp = find_most_recent_time(department_data)
+        upload_csv(
+            department_data, "department", "kp-northcoder-ingestion-bucket"
+        )
+        write_updated_time(updated_timestamp, "department")
 
-design_data = get_design_add()
+    design_data = get_design_add()
 
-if len(design_data) > 0:
-    updated_timestamp = find_most_recent_time(design_data)
-    upload_csv(design_data, "design", "kp-northcoder-ingestion-bucket")
-    write_updated_time(updated_timestamp, "design")
+    if len(design_data) > 0:
+        updated_timestamp = find_most_recent_time(design_data)
+        upload_csv(design_data, "design", "kp-northcoder-ingestion-bucket")
+        write_updated_time(updated_timestamp, "design")
 
-payment_data = get_payment_add()
+    payment_data = get_payment_add()
 
-if len(payment_data) > 0:
-    updated_timestamp = find_most_recent_time(payment_data)
-    upload_csv(payment_data, "payment", "kp-northcoder-ingestion-bucket")
-    write_updated_time(updated_timestamp, "payment")
+    if len(payment_data) > 0:
+        updated_timestamp = find_most_recent_time(payment_data)
+        upload_csv(payment_data, "payment", "kp-northcoder-ingestion-bucket")
+        write_updated_time(updated_timestamp, "payment")
 
-purchase_order_data = get_purchase_order_add()
+    purchase_order_data = get_purchase_order_add()
 
-if len(purchase_order_data) > 0:
-    updated_timestamp = find_most_recent_time(purchase_order_data)
-    upload_csv(
-        purchase_order_data, "purchase_order", "kp-northcoder-ingestion-bucket"
-    )
-    write_updated_time(updated_timestamp, "purchase_order")
+    if len(purchase_order_data) > 0:
+        updated_timestamp = find_most_recent_time(purchase_order_data)
+        upload_csv(
+            purchase_order_data,
+            "purchase_order",
+            "kp-northcoder-ingestion-bucket",
+        )
+        write_updated_time(updated_timestamp, "purchase_order")
 
-sales_order_data = get_sales_order_add()
+    sales_order_data = get_sales_order_add()
 
-if len(sales_order_data) > 0:
-    updated_timestamp = find_most_recent_time(sales_order_data)
-    upload_csv(
-        sales_order_data, "sales_order", "kp-northcoder-ingestion-bucket"
-    )
-    write_updated_time(updated_timestamp, "sales_order")
+    if len(sales_order_data) > 0:
+        updated_timestamp = find_most_recent_time(sales_order_data)
+        upload_csv(
+            sales_order_data, "sales_order", "kp-northcoder-ingestion-bucket"
+        )
+        write_updated_time(updated_timestamp, "sales_order")
 
-staff_data = get_staff_add()
+    staff_data = get_staff_add()
 
-if len(staff_data) > 0:
-    updated_timestamp = find_most_recent_time(staff_data)
-    upload_csv(staff_data, "staff", "kp-northcoder-ingestion-bucket")
-    write_updated_time(updated_timestamp, "staff")
+    if len(staff_data) > 0:
+        updated_timestamp = find_most_recent_time(staff_data)
+        upload_csv(staff_data, "staff", "kp-northcoder-ingestion-bucket")
+        write_updated_time(updated_timestamp, "staff")
