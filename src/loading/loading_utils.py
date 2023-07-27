629c79b1790e64249aacb4177d5f9758390b1740
LisaSco <lisascotney@hotmail.com>
2023-07-27 11:44:57 +0100

    loading utils finished

--- src/loading/loading_utils.py
+++ src/loading/loading_utils.py
@@ -78,3 +78,92 @@ def insert_into_dim_staff(conn, staff_data):
     conn.close()
 
     return loaded_data
+
+
+def insert_into_dim_location(conn, location_data):
+    """
+    Insert data into the dim_location table
+
+    :param conn: pg8000 connection 
+    :param location_data: list of lists containing data to be inserted
+    """
+
+    for location in location_data:
+        conn.run("INSERT INTO dim_location (location_id, address_line_1, address_line_2, district, city, postal_code, country, phone) VALUES (:(location_id), :(address_line_1, :(address_line_2), :(district), :(city), :(postal_code), :(country), :(phone))",
+                 location_id=location[0], address_line_1=location[1], address_line_2=location[2], district=location[3], city=location[4], postal_code=location[5], country=location[6], phone=location[7])
+
+    loaded_data = []
+    for row in conn.run("SELECT * FROM location_data"):
+        loaded_data.append(row)
+
+    conn.close()
+
+    return loaded_data
+
+
+def insert_into_dim_date(conn, date_data):
+    """
+    Insert data into the dim_date table
+
+    :param conn: pg8000 connection 
+    :param date_data: list of lists containing data to be inserted
+    """
+
+    for date in date_data:
+        conn.run("INSERT INTO dim_date (date_id, year, month, day, day_of_week, day_name, month_name, quarter) VALUES (:(date_id), :(year), :(month), :(day), :(day_of_week), :(day_name), :(month_name), :(quarter))",
+                 date_id=date[0], year=date[1], month=date[2], day=date[3], day_of_week=date[4], day_name=date[5], month_name=date[6], quarter=date[7])
+
+    loaded_data = []
+    for row in conn.run("SELECT * FROM date_data"):
+        loaded_data.append(row)
+
+    conn.close()
+
+    return loaded_data
+
+
+def insert_into_dim_counterparty(conn, counterparty_data):
+    """
+    Insert data into the dim_counterparty table
+
+    :param conn: pg8000 connection 
+    :param counterparty_data: list of lists containing data to be inserted
+    """
+
+    for counterparty in counterparty_data:
+        conn.run("INSERT INTO dim_counterparty (counterparty_id, counterparty_legal_name, counterparty_legal_address_line_1, counterparty_legal_address_line_2, counterparty_legal_district, counterparty_legal_city, counterparty_legal_postal_code, counterparty_legal_country, counterparty_legal_phone_number) VALUES (:(counterparty_id), :(counterparty_legal_name), :(counterparty_legal_address_line_1), :(counterparty_legal_address_line_2), :(counterparty_legal_district), :(counterparty_legal_city), :(counterparty_legal_postal_code), :(counterparty_legal_country), :(counterparty_legal_phone_number))",
+                 counterparty_id=counterparty[0], counterparty_legal_name=counterparty[1], counterparty_legal_address_line_1=counterparty[2], counterparty_legal_address_line_2=counterparty[3], counterparty_legal_district=counterparty[4], counterparty_legal_city=counterparty[5], counterparty_legal_postal_code=counterparty[6], counterparty_legal_country=counterparty[7], counterparty_legal_phone_number=counterparty[8])
+
+    loaded_data = []
+    for row in conn.run("SELECT * FROM counterparty_data"):
+        loaded_data.append(row)
+
+    conn.close()
+
+    return loaded_data
+
+
+def insert_into_dim_fact_sales_order(conn, fact_sales_order_data):
+    """
+    Insert data into the dim_fact_sales_order table
+
+    :param conn: pg8000 connection 
+    :param fact_sales_order_data: list of lists containing data to be inserted
+    """
+
+    for fact in fact_sales_order_data:
+        conn.run("INSERT INTO fact_sales_order (sales_record_id, sales_order_id, created_date, created_time, last_updated_date, last_updated_time, sales_staff_id, counterparty_id, units_sold, unit_price, currency_id, design_id, agreed_payment_date, agreed_delivery_date, agreed_delivery_location_id) VALUES (:(sales_record_id), :(sales_order_id), :(created_date), :(created_time), :(last_updated_date), :(last_updated_time), :(sales_staff_id), :(counterparty_id), :(units_sold), :(unit_price), :(currency_id), :(design_id), :(agreed_payment_date), :(agreed_delivery_date), :(agreed_delivery_location_id))",
+                 sales_record_id=fact[0], sales_order_id=fact[1], created_date=fact[2], created_time=fact[3], last_updated_date=fact[4], last_updated_time=fact[5], sales_staff_id=fact[6], counterparty_id=fact[7], units_sold=fact[8], unit_price=fact[9], currency_id=fact[10], design_id=fact[11], agreed_payment_date=fact[12], agreed_delivery_date=fact[13], agreed_delivery_location_id=fact[14])
+
+    loaded_data = []
+    for row in conn.run("SELECT * FROM fact_sales_order_data"):
+        loaded_data.append(row)
+
+    conn.close()
+
+    return loaded_data
+
+
+
+
+
