f3cf0c6d5cdfdc096aea5b0d8df452697f997fe0
Matthew Heath <mj.heath@outlook.com>
2023-07-27 11:01:31 +0100

    corrects test file to pass flake tests

--- tests/test_get_address_add.py
+++ tests/test_get_address_add.py
@@ -1,46 +1,45 @@
-from pg8000 import VARCHAR
-from requests import patch
 from src.ingestion_lambda.utils.get_address_add import get_address_add
-from unittest.mock import patch, Mock
-from unittest import mock
-import pg8000.native
+from unittest.mock import patch
 import datetime
-import pytest
+
+ingestion_utils_path = 'src.ingestion_lambda.utils'
+address_get_last_time_path = 'get_address_add.get_last_time'
+get_last_time_patch_path = ingestion_utils_path + address_get_last_time_path
+
 
 def test_get_address_add_returns_list():
-    with patch('src.ingestion_lambda.utils.get_address_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
-        '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
-        # test that we are returning a list,
+            '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
         result = get_address_add()
-        # print(result)
         assert isinstance(result, list)
 
 
 def test_get_address_add_returns_correct_keys():
-    with patch('src.ingestion_lambda.utils.get_address_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
-        '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
-        # test that we have the correct keys
+            '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
         expected_keys = {
-            "location_id", "address_line_1", "address_line_2", "district", "city",
-            "postal_code", "country", "phone", "created_at", "last_updated"}
+            "location_id", "address_line_1",
+            "address_line_2", "district", "city",
+            "postal_code", "country", "phone",
+            "created_at", "last_updated"}
         result = get_address_add()
         assert all(set(item.keys()) == expected_keys for item in result)
 
 
 def test_get_address_add_has_correct_value_types():
-    with patch('src.ingestion_lambda.utils.get_address_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
-        '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
-    # test that each key has the correct type value
+            '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
         result = get_address_add()
         for item in result:
             assert isinstance(item['location_id'], int)
             assert isinstance(item['address_line_1'], str)
-            assert isinstance(item['address_line_2'],
-                            str) or item['address_line_2'] is None
-            assert isinstance(item['district'], str) or item['district'] is None
+            assert isinstance(item['address_line_2'], str) or \
+                item['address_line_2'] is None
+            assert isinstance(item['district'], str) or \
+                item['district'] is None
             assert isinstance(item['city'], str)
             assert isinstance(item['postal_code'], str)
             assert isinstance(item['country'], str)
@@ -50,9 +49,8 @@ def test_get_address_add_has_correct_value_types():
 
 
 def test_get_address_calls_get_last_time():
-    #test that the SQL query calls get_last_time()
-    with patch('src.ingestion_lambda.utils.get_address_add.get_last_time') as mock_get_last_time:
+    with patch(get_last_time_patch_path) as mock_get_last_time:
         mock_get_last_time.return_value = datetime.datetime.strptime(
-        '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
+            '2020-07-25 15:20:49.962000', '%Y-%m-%d %H:%M:%S.%f')
         get_address_add()
         assert mock_get_last_time.call_count == 1
