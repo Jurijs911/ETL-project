810cb1dfc11903b71a48ab0d4e62902c300b96ae
Jurijs911 <jurijs.kosticins@gmail.com>
2023-07-31 10:59:30 +0100

    refactored tests for get_address to work with test DB

--- src/ingestion_lambda/get_address_add.py
+++ src/ingestion_lambda/get_address_add.py
@@ -20,15 +20,15 @@ class MissingRequiredEnvironmentVariables (Exception):
         self.password = db_password
 
 
-def get_address_add():
+def get_address_add(db_user=os.environ.get("DB_SOURCE_USER"),
+                    db_schema='',
+                    db_database=os.environ.get("DB_SOURCE_NAME"),
+                    db_host=os.environ.get("DB_SOURCE_HOST"),
+                    db_port=os.environ.get("DB_SOURCE_PORT"),
+                    db_password=os.environ.get("DB_SOURCE_PASSWORD")):
     """
     CONNECTION
     """
-    db_user = os.environ.get("DB_SOURCE_USER")
-    db_database = os.environ.get("DB_SOURCE_NAME")
-    db_host = os.environ.get("DB_SOURCE_HOST")
-    db_port = os.environ.get("DB_SOURCE_PORT")
-    db_password = os.environ.get("DB_SOURCE_PASSWORD")
 
     if not all([db_user, db_database, db_host, db_port, db_password]):
         raise MissingRequiredEnvironmentVariables(
@@ -53,11 +53,14 @@ def get_address_add():
     """
     QUERY DATA CREATED IN LAST SEARCH INTERVAL
     """
-    query = f"SELECT * FROM address WHERE created_at > :search_interval;"
+    target_table = db_schema + 'address'
+    print(target_table)
+    query = (
+        f'SELECT * FROM {target_table} WHERE created_at > :search_interval;')
     params = {'search_interval': search_interval}
     rows = conn.run(query, **params)
-# #if rows = {}:
-# log....
+    # #if rows = {}:
+    # log....
 
     created_data = []
     for row in rows:
@@ -74,5 +77,4 @@ def get_address_add():
             "last_updated": row[9],
         }
         created_data.append(item)
-    print(created_data)
     return created_data
