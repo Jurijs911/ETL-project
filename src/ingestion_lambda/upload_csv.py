c5067139c6011156a2cdee9665864b16b3019aca
CParso <cameronparsonage09@gmail.com>
2023-08-01 10:43:21 +0100

    refactor upload csv to handle missing csv file

--- src/ingestion_lambda/upload_csv.py
+++ src/ingestion_lambda/upload_csv.py
@@ -1,24 +1,27 @@
 import csv
 import boto3
 import os
+from botocore.exceptions import ClientError
 
 
 def upload_csv(data, table_name, bucket_name):
     s3_client = boto3.client("s3")
 
-    downloaded_csv = (
-        s3_client.get_object(Bucket=bucket_name, Key=f"{table_name}.csv")[
-            "Body"
-        ]
-        .read()
-        .decode("utf-8")
-        .split("\r\n")
-    )
-
-    with open(f"{table_name}.csv", "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        for row in downloaded_csv:
-            writer.writerow([row])
+    try:
+        downloaded_csv = (
+            s3_client.get_object(Bucket=bucket_name, Key=f"{table_name}.csv")[
+                "Body"
+            ]
+            .read()
+            .decode("utf-8")
+            .split("\r\n")
+        )
+        with open(f"{table_name}.csv", "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            for row in downloaded_csv:
+                writer.writerow([row])
+    except ClientError:
+        pass
 
     with open(f"{table_name}.csv", "a", newline="") as csvfile:
         if len(data) > 0:
