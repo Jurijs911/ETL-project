06d17aa5fb98099bbf05379e89d67b43c38a696d
CParso <cameronparsonage09@gmail.com>
2023-08-01 11:30:31 +0100

    Refactor get_last_time and upload_csv

--- src/ingestion_lambda/get_last_time.py
+++ src/ingestion_lambda/get_last_time.py
@@ -3,17 +3,15 @@ import boto3
 
 
 def get_last_time(bucket):
-
     s3_client = boto3.client("s3")
-# add try block
+    # add try block
     # Read file from bucket
     bucket_name = "kp-northcoder-ingestion-bucket"
     key = f"{bucket}/created_at.txt"
 
     file = s3_client.get_object(Bucket=bucket_name, Key=key)
 
-    file_content = file['Body'].read().decode('utf-8')
-    print(file_content)
+    file_content = file["Body"].read().decode("utf-8")
 
     # returns
 
