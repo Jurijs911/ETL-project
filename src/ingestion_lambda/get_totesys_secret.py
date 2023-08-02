5365f4fe19d5f91f705a9d8d79d2a5b086d19815
CParso <cameronparsonage09@gmail.com>
2023-08-02 15:16:05 +0100

    add util for retrieving db secrets from secrets manager

---
+++ src/ingestion_lambda/get_totesys_secret.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+    secret_name = "totesys_DB_access"
+    region_name = "eu-west-2"
+
+    client = boto3.client(
+        service_name="secretsmanager", region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response["SecretString"]
+
+    return json.loads(secret)
