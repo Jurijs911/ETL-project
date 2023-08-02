058561590ab14495991a5dcd74e4267fc28dad60
CParso <cameronparsonage09@gmail.com>
2023-08-02 16:16:33 +0100

    fix: test github actions connection to aws

--- src/ingestion_lambda/get_totesys_secret.py
+++ src/ingestion_lambda/get_totesys_secret.py
@@ -9,7 +9,7 @@ def get_secret():
 
     s3_client = boto3.client("s3", region_name=region_name)
 
-    s3_client.list_objects_v2("kp-northcoders-ingestion-bucket")
+    s3_client.list_objects_v2(Bucket="kp-northcoders-ingestion-bucket")
 
     client = boto3.client(
         service_name="secretsmanager", region_name=region_name
