af1765259387be782f16d805df1b0554fd322040
CParso <cameronparsonage09@gmail.com>
2023-08-07 10:50:39 +0100

    change eventbridge timing to every 10 minutes

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,12 +1,12 @@
-resource "aws_cloudwatch_event_rule" "every_5_minutes" {
-  name        = "every_5_minutes_rule"
-  description = "trigger lambda every 5 minute"
+resource "aws_cloudwatch_event_rule" "every_10_minutes" {
+  name        = "every_10_minutes_rule"
+  description = "trigger lambda every 10 minute"
 
-  schedule_expression = "rate(5 minutes)"
+  schedule_expression = "rate(10 minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule      = aws_cloudwatch_event_rule.every_5_minutes.name
+  rule      = aws_cloudwatch_event_rule.every_10_minutes.name
   target_id = "SendToLambda"
   arn       = aws_lambda_function.ingestion_lambda.arn
 }
@@ -16,5 +16,5 @@ resource "aws_lambda_permission" "allow_eventbridge" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ingestion_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.every_5_minutes.arn
+  source_arn    = aws_cloudwatch_event_rule.every_10_minutes.arn
 }
