d3909da20b6ad9c1aea414a1cd7ce4075c2d05c3
CParso <cameronparsonage09@gmail.com>
2023-08-07 15:37:32 +0100

    add aws alarm

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -1,13 +1,27 @@
-# resource "aws_cloudwatch_log_metric_filter" "alert_error" {
-#   name           = "alert_error_metric_filter"
-#   pattern        = "ERROR"
-#   log_group_name = "aws/lambda/${aws_lambda_function.ingestion_lambda.function_name}"
+resource "aws_cloudwatch_log_metric_filter" "alert_error" {
+  name           = "alert_error_metric_filter"
+  pattern        = "ERROR"
+  log_group_name = "aws/lambda/${aws_lambda_function.ingestion_lambda.function_name}"
+
+  metric_transformation {
+    name      = "ErrorCount"
+    namespace = "AWS/EC2"
+    value     = "1"
+  }
+}
 
 
-# resource "aws_cloudwatch_log_metric_filter" "alert_error" {
-#   name           = "alert_error_metric_filter"
-#   pattern        = "ERROR"
-#   log_group_name = "aws/lambda/${aws_lambda_function.remodelling_lambda.function_name}"
+resource "aws_cloudwatch_log_metric_filter" "alert_error" {
+  name           = "alert_error_metric_filter"
+  pattern        = "ERROR"
+  log_group_name = "aws/lambda/${aws_lambda_function.remodelling_lambda.function_name}"
+
+  metric_transformation {
+    name      = "ErrorCount"
+    namespace = "AWS/EC2"
+    value     = "1"
+  }
+}
 
 
 # resource "aws_cloudwatch_log_metric_filter" "alert_error" {
@@ -23,16 +37,16 @@
 #   }
 # }
 
-# resource "aws_cloudwatch_metric_alarm" "alert_errors" {
-#   alarm_name          = "error_alarm"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = 1
-#   metric_name         = "ErrorCount"
-#   namespace           = "AWS/EC2"
-#   period              = 300
-#   statistic           = "Average"
-#   threshold           = 1
-#   alarm_description   = "This alarm is triggered when ERROR appears"
-#   actions_enabled     = "true"
-#   alarm_actions       = [aws_sns_topic.error_notification.arn]
-# }
+resource "aws_cloudwatch_metric_alarm" "alert_errors" {
+  alarm_name          = "error_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ErrorCount"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 1
+  alarm_description   = "This alarm is triggered when ERROR appears"
+  actions_enabled     = "true"
+  alarm_actions       = [aws_sns_topic.error_notification.arn]
+}
