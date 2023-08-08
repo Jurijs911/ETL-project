8ee607f6b2765eec12126523486a9dd145e56bba
CParso <cameronparsonage09@gmail.com>
2023-08-08 11:07:19 +0100

    fix secret_name in get_secret loading

--- src/loading/get_secret.py
+++ src/loading/get_secret.py
@@ -4,7 +4,7 @@ from botocore.exceptions import ClientError
 
 
 def get_secret():
-    secret_name = "Target_DB_access"
+    secret_name = "arn:aws:secretsmanager:eu-west-2:124301163178:secret:Target_DB_Access-CLApPX"
     region_name = "eu-west-2"
 
     client = boto3.client(
