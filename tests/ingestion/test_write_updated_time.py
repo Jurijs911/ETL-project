0d4fa788b2d7ae21fb1bbaeb6d840e2c59cfa270
zenabhaider <zenabhaider05@gmail.com>
2023-08-08 12:10:02 +0100

    added doc string to write updated time test and moved into tests/ingestion older

--- tests/ingestion/test_write_updated_time.py
+++ tests/ingestion/test_write_updated_time.py
@@ -5,6 +5,18 @@ import boto3
 
 @mock_s3
 def test_write_updated_time_returns_status_code_when_successfully_uploading():
+    """
+    Test the write_updated_time function for successful uploading.
+
+    This function tests the behavior of the write_updated_time function when
+    it successfully uploads a timestamp to Amazon S3. It uses the moto library
+    to mock the Amazon S3 service and then calls the write_updated_time
+    function with test data. The HTTP status code of the response is asserted
+    to check it was successful (200 OK).
+
+    Raises:
+        AssertionError: If the assertions fail.
+    """
     s3_client = boto3.client("s3")
     s3_client.create_bucket(
         Bucket="kp-northcoders-ingestion-bucket",
